cdff8eb76d517a326e407a92ce03ebe8d9c59353
pliok <37558728+pliok@users.noreply.github.com>
2018-04-05 17:37:11 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/18rnX-yoX6Q-1TfBghFRrUi2bKm6NJZN_DikAa1KU4UE/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1fQ3MX1Q2ymqzIMTd7fuCvUhcUDOba-UTHn7sgXEq_MQ/edit#gid=0';
